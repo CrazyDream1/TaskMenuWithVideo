818d6eedf83acbf46aa0aa443c87378cdae594cc
CrazyDream1 <35272226+CrazyDream1@users.noreply.github.com>
2023-06-01 21:07:14 +0300

    Basics and plagins

---
+++ TaskMenuWithVideo/Assets/Scripts/GameCore/ScriptableObjects/Settings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace GameCore.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NenSettings", menuName = "Settings", order = 51)]
+    public class Settings : ScriptableObject
+    {
+        [SerializeField] public bool IsMusicEnabled;
+        [SerializeField] public bool IsSoundEnabled;
+        [SerializeField] [Range(0, 1f)] public float MusicAndSoundVolume = 1;
+        [SerializeField] public string StreamUrl;
+    }
+}
---
+++ TaskMenuWithVideo/Assets/Scripts/GameCore/SettingsController.cs
@@ -0,0 +1,40 @@
+﻿using GameCore.ScriptableObjects;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class SettingsController : MonoBehaviour
+    {
+        [SerializeField] private Settings _settings;
+        
+        public bool IsMusicEnabled => _settings.IsMusicEnabled;
+        public bool IsSoundEnabled => _settings.IsSoundEnabled;
+        public float Volume => _settings.MusicAndSoundVolume;
+        public string StreamUrl => _settings.StreamUrl;
+        
+        public void ChangeMusicState()
+        {
+            _settings.IsMusicEnabled = !_settings.IsMusicEnabled;
+            OnMusicChangedEvent?.Invoke();
+        }
+        
+        public void ChangeSoundState()
+        {
+            _settings.IsSoundEnabled = !_settings.IsMusicEnabled;
+            OnSoundChangedEvent?.Invoke();
+        }
+
+        public void ChangeVolume(float volume)
+        {
+            _settings.MusicAndSoundVolume = volume;
+            OnVolumeChangedEvent?.Invoke(volume);
+        }
+        
+        public delegate void OnChangeHandler();
+        public event OnChangeHandler OnMusicChangedEvent;
+        public event OnChangeHandler OnSoundChangedEvent;
+
+        public delegate void OnVolumeChangeHandler(float newVolume);
+        public event OnVolumeChangeHandler OnVolumeChangedEvent;
+    }
+}
